8fa84e11e92531ace0357f40508ac5095aa1766e
Vy Nguyen <vynguyen9699@gmail.com>
2020-09-24 20:56:14 +0300

    add bottom navigation and create basic fragments

--- app/src/main/java/com/example/steptracker/MainActivity.kt
+++ app/src/main/java/com/example/steptracker/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.steptracker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val todayFragment = TodayFragment()
+        val reportFragment = ReportFragment()
+        val healthFragment = HealthFragment()
+        val moreFragment = MoreFragment()
+        //Set one of the items already selected to show on the screen
+        if (savedInstanceState == null) {
+            bottom_navigation.selectedItemId = R.id.b1_today
+        }
+        //Set a listener that will be notified when a bottom navigation item is selected
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.b1_today -> setCurrentFragment(todayFragment)
+                R.id.b2_report -> setCurrentFragment(reportFragment)
+                R.id.b3_health -> setCurrentFragment(healthFragment)
+                R.id.b4_more -> setCurrentFragment(moreFragment)
+            }
+            true
+        }
     }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_fragment, fragment)
+            commit()
+        }
 }
+
