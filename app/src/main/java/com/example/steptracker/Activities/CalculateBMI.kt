859b1bd5d46468f051ce6f50509247668c922e94
RenMai <maithieunhan@gmail.com>
2020-09-26 19:27:36 +0300

    read write file

--- app/src/main/java/com/example/steptracker/Activities/CalculateBMI.kt
+++ app/src/main/java/com/example/steptracker/Activities/CalculateBMI.kt
@@ -1,5 +1,6 @@
 package com.example.steptracker.Activities
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -17,9 +18,30 @@ class CalculateBMI : AppCompatActivity() {
             calculateBMI()
         }
     }
-    fun calculateBMI() {
+    private fun calculateBMI() {
+        if(weightValue.text.isNullOrEmpty() || heightValue.text.isNullOrEmpty()){
+            return
+        }
         var weight = weightValue.text.toString().toFloat()
         var height = heightValue.text.toString().toFloat()
         bmiValue.text = (weight/(height*height)).toString()
+        writeDataToFile()
+    }
+    private fun writeDataToFile()
+    {
+        //mode private = rewrite the file. mode_append = add content to the file
+        this!!.openFileOutput(dataFile, Context.MODE_PRIVATE).use {
+            it.write("${weightValue.text}\n".toByteArray())
+            it.write("${heightValue.text}\n".toByteArray())
+        }
+    }
+    private fun readDataFromFile()
+    {
+        this!!.openFileInput(dataFile)?.bufferedReader()?.useLines{
+                lines -> lines.forEach { dataFileList.add(it) }
+            weightValue.setText(dataFileList[0])
+            heightValue.setText(dataFileList[1])
+            calculateBMI()
+        }
     }
 }
