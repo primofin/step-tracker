dba10bb373ab7c9cb2ddaa4f6be2d6811227326f
RenMai <maithieunhan@gmail.com>
2020-09-26 21:40:25 +0300

    logged by google

--- app/src/main/java/com/example/steptracker/Activities/MainActivity.kt
+++ app/src/main/java/com/example/steptracker/Activities/MainActivity.kt
@@ -3,15 +3,38 @@ package com.example.steptracker.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+
 import com.example.steptracker.R
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("534333492380-163qq0hl74sui67kgbinj0vjce2surbd.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        google_button.setOnClickListener {
+            signIn()
+        }
     }
     fun toCounterActivity(view: View){
         val intent = Intent(this, CounterStep::class.java)
@@ -20,6 +43,76 @@ class MainActivity : AppCompatActivity() {
     fun toBMIActivity(view: View){
         val intent = Intent(this, CalculateBMI::class.java)
         startActivity(intent)
+    }
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(
+            signInIntent, RC_SIGN_IN
+        )
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
+                // Update your UI here
+            }
+    }
+
+    private fun revokeAccess() {
+        mGoogleSignInClient.revokeAccess()
+            .addOnCompleteListener(this) {
+                // Update your UI here
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(
+                ApiException::class.java
+            )
+            // Signed in successfully
+            val googleId = account?.id ?: ""
+            Log.i("Google ID",googleId)
+
+            val googleFirstName = account?.givenName ?: ""
+            Log.i("Google First Name", googleFirstName)
+
+            val googleLastName = account?.familyName ?: ""
+            Log.i("Google Last Name", googleLastName)
+
+            val googleEmail = account?.email ?: ""
+            Log.i("Google Email", googleEmail)
+
+            val googleProfilePicURL = account?.photoUrl.toString()
+            Log.i("Google Profile Pic URL", googleProfilePicURL)
+
+            val googleIdToken = account?.idToken ?: ""
+            Log.i("Google ID Token", googleIdToken)
+
+
+            val myIntent = Intent(this, DetailsActivity::class.java)
+            myIntent.putExtra("google_id", googleId)
+            myIntent.putExtra("google_first_name", googleFirstName)
+            myIntent.putExtra("google_last_name", googleLastName)
+            myIntent.putExtra("google_email", googleEmail)
+            myIntent.putExtra("google_profile_pic_url", googleProfilePicURL)
+            myIntent.putExtra("google_id_token", googleIdToken)
+            this.startActivity(myIntent)
+        } catch (e: ApiException) {
+            // Sign in was unsuccessful
+            Log.e(
+                "failed code=", e.statusCode.toString()
+            )
+        }
     }
 
 }
