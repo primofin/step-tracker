6808f1f9c3e92c876e86382107fe9fbaf0e1f7f6
RenMai <maithieunhan@gmail.com>
2020-09-28 18:09:49 +0300

    fix data live

--- app/src/main/java/com/example/steptracker/Model/BMIViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.steptracker.Model
-
-import androidx.lifecycle.ViewModel
-
-class BMIViewModel: ViewModel() {
-    var liveWeight  = 0 as Float
-    var liveHeight = 0 as Float
-    var liveBMI  = 0 as Float
-}
